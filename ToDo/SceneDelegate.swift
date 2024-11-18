ea9e75dc922e016f3f3e17361291d89373dd6a9c
valeriy <vlrp512@gmail.com>
2024-11-18 15:34:05 +0500

    Add project

---
+++ ToDo/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  ToDo
+//
+//  Created by Valeriy P on 18.11.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ ToDo/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Valeriy P on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
