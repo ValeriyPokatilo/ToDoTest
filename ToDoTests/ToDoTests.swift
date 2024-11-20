d8eb2db288f814ad93c88143fa40dcfd9cbd8625
valeriy <vlrp512@gmail.com>
2024-11-20 13:56:38 +0500

    Add tests target

---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Valeriy P on 20.11.2024.
+//
+
+import Testing
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
